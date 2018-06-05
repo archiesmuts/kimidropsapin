cf0e8da385fdeb1ec5c5f47ae353bdadc6226d46
Archie Smuts <archiesmuts@gmail.com>
2018-06-05 18:35:06 +0200

    reverse changes for devise mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,17 +63,17 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "kimisblog_#{Rails.env}"
   # Setup the mailer config
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-   :user_name => ENV['SENDGRID_USERNAME'],
-   :password => ENV['SENDGRID_PASSWORD'],
-   :domain => 'kimisblog.herokuapp.com',
-   :address => 'smtp.sendgrid.net',
-   :port => 587,
-   :authentication => :plain,
-   :enable_starttls_auto => true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.smtp_settings = {
+  #  :user_name => ENV['SENDGRID_USERNAME'],
+  #  :password => ENV['SENDGRID_PASSWORD'],
+  #  :domain => 'kimisblog.herokuapp.com',
+  #  :address => 'smtp.sendgrid.net',
+  #  :port => 587,
+  #  :authentication => :plain,
+  #  :enable_starttls_auto => true
+  # }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
