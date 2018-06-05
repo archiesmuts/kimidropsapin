b02220032ff3f3450919712eb78254f286f3c198
Archie Smuts <archiesmuts@gmail.com>
2018-06-05 18:43:49 +0200

    set up the mailer config in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,18 +62,18 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "kimisblog_#{Rails.env}"
-  # Setup the mailer config
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.smtp_settings = {
-  #  :user_name => ENV['SENDGRID_USERNAME'],
-  #  :password => ENV['SENDGRID_PASSWORD'],
-  #  :domain => 'kimisblog.herokuapp.com',
-  #  :address => 'smtp.sendgrid.net',
-  #  :port => 587,
-  #  :authentication => :plain,
-  #  :enable_starttls_auto => true
-  # }
+  #Setup the mailer config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+   :user_name => ENV['SENDGRID_USERNAME'],
+   :password => ENV['SENDGRID_PASSWORD'],
+   :domain => 'kimisblog.herokuapp.com',
+   :address => 'smtp.sendgrid.net',
+   :port => 587,
+   :authentication => :plain,
+   :enable_starttls_auto => true
+  }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
