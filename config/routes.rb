0969b59a0b28c9f8c55bce052076aa087f97ed40
Archie Smuts <archiesmuts@gmail.com>
2018-06-06 19:32:24 +0200

    removed routes restrictions for comments under post

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show]
   resources :posts do
-    resources :comments, only: [:index, :destroy]
+    resources :comments #, only: [:index, :new, :create, :destroy]
   end
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
