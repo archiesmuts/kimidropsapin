9251a64150549e1c7850b477ccfbe84dbd808ab7
Archie Smuts <archiesmuts@gmail.com>
2019-01-05 15:55:31 +0200

    add post published version span helper for show and index views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,17 @@ module PostsHelper
   include ActsAsTaggableOn::TagsHelper
   include Pagy::Frontend
 
+  def post_version_label(published)
+    version_span_generator(published)
+  end
+
+  private
+  def version_span_generator(published)
+    case published
+    when 'draft'
+      content_tag(:span, "Draft", class: 'badge badge-warning')
+    when 'final'
+      content_tag(:span, "Final", class: 'badge badge-success')
+    end
+  end
 end
