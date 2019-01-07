874780b294562fade6cde61b715523bb62c627e8
Archie Smuts <archiesmuts@gmail.com>
2019-01-07 16:44:34 +0200

    change photo album index to show only published photos

--- app/controllers/all_photos_controller.rb
+++ app/controllers/all_photos_controller.rb
@@ -1,7 +1,7 @@
 class AllPhotosController < ApplicationController
   layout "application_tags"
   def index
-    @all_photos = Post.order("created_at DESC")
+    @published_posts = Post.final.order("created_at DESC")
     # @pagy, @records = pagy(Post.order("created_at DESC"), items: 6)
   end
 end
